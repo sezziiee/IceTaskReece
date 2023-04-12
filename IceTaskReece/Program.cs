791716057895050a5f312566cfa13b0a900f7b8b
lab_services_student <lab_services_student@lab000000>
2023-04-12 11:30:21 +0000

    Added hello message

--- IceTaskReece/Program.cs
+++ IceTaskReece/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello class how is the weather?");
         }
     }
 }
